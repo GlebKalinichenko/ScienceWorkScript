5df67a5c13e82dfb8205bd07e00ae2dd22db13bf
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-04 17:57:37 +0300

    Select records from table Conferences

---
+++ SelectQuery/ConferenceScript.php
@@ -0,0 +1,16 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+	$mysqli = new mysqli("localhost", "root", "1111", "ScienceWorks");
+    $data = array();
+    $mysqli->query('SET NAMES UTF8');
+	$result = $mysqli->query("SELECT * FROM Conferences");
+    	if ($result->num_rows > 0) {
+ 	   // output data of each row
+    	while($row = $result->fetch_assoc()) {
+            $data[] = $row;
+    	}
+        echo json_encode($data);
+	}
+	
+?>
