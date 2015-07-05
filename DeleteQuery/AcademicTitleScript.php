232232adecf8f91efe6db735e4d946702f6a7288
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-05 14:56:45 +0300

    Delete record from table AcademicTitles

---
+++ DeleteQuery/AcademicTitleScript.php
@@ -0,0 +1,16 @@
+<?php
+	$data = file_get_contents('php://input');
+	$json = json_decode($data);
+	$academicTitle = $json->{'academicTitle'};
+	$mysqli = new mysqli("localhost", "root", "1111", "ScienceWorks");
+	/* проверка соединения */
+	if (mysqli_connect_errno()) {
+    	printf("Соединение не установлено: %s\n", mysqli_connect_error());
+    	exit();
+	}
+	$query = "DELETE FROM AcademicTitles  WHERE AcademicTitle = '$academicTitle'";
+	$mysqli->query($query);
+
+	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
+
+?>
